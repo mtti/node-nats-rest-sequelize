c61fb1d1adaae2e1573d4f422b0de567d7db82b7
mtti <mtti@users.noreply.github.com>
2018-03-07 20:04:25 +0200

    Separate instance and collection actions, automatically fetch model instance for instance actions

--- src/resource.js
+++ src/resource.js
@@ -16,8 +16,8 @@ class SequelizeResource {
           .map(pair => new SequelizeResource(
             service.natsClient,
             pair[1],
-            { logger: service.logger, actions: actions[pair[0]]})
-          )
+            { logger: service.logger }
+          ))
           .forEach((resource) => {
             resource.start();
           });
@@ -34,9 +34,11 @@ class SequelizeResource {
       this._logger = options.logger;
     }
 
-    this._handlers = {};
+    this._collectionActions = {};
+    this._instanceActions = {};
+
     if (options.defaultHandlers !== false) {
-      this._handlers = {
+      this._instanceActions = {
         GET: this._get.bind(this),
         PUT: this._put.bind(this),
         PATCH: this._patch.bind(this),
@@ -44,13 +46,25 @@ class SequelizeResource {
       };
     }
 
-    if (options.actions) {
-      _.merge(this._handlers, options.actions);
+    if (this._model.collectionActions) {
+      _.merge(this._collectionActions, this._model.collectionActions);
+    }
+    if (this._model.instanceActions) {
+      const wrappedInstanceActions = _.fromPairs(
+        _.toPairs(this._model.instanceActions)
+          .map(pair => [pair[0], this._wrapInstanceAction(pair[1])])
+      );
+      _.merge(this._instanceActions, wrappedInstanceActions);
     }
   }
 
   start() {
-    this._server = new ResourceServer(this._natsClient, this._name, this._handlers, this._logger);
+    const serverOptions = {
+      collectionActions: this._collectionActions,
+      instanceActions: this._instanceActions,
+      logger: this._logger,
+    };
+    this._server = new ResourceServer(this._natsClient, this._name, serverOptions);
 
     this._model.hook('afterSave', (instance, options) => {
       this._server.emit(instance.toJSON());
@@ -110,6 +124,22 @@ class SequelizeResource {
     }
     return this._model.destroy({ where: { id }})
       .then(() => null);
+  }
+
+  /**
+   * Wrap an instance action callback so that the instance it's targeting is automatically loaded
+   * and passed to the callback instead of the ID.
+   */
+  _wrapInstanceAction(action) {
+    return (id, body) => {
+      return this._model.findById(id)
+        .then((instance) => {
+          if (instance === null) {
+            return Promise.reject(createError(404));
+          }
+          return action(instance, body);
+        });
+    }
   }
 }
 
